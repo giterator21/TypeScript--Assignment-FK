a11891a90fa0282e74e7c109d0013eb002c73b01
giterator21 <fel.kopp.21@lehre.mosbach.dhbw.de>
2022-05-11 00:47:09 +0200

    Canvas nachgeschoben, getter f Canvas implemetiert

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// Hier wird auf das Canvas "gameCanvas" aus der CSS-Datei über "getElementById" in TypeScript zugegriffen
+const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+canvas.width = 900;
+canvas.height = 750;
