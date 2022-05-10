cd8bbe4f8c49e907502a6c570cc52d7bcf69d3f5
giterator21 <fel.kopp.21@lehre.mosbach.dhbw.de>
2022-05-11 01:01:09 +0200

    HIntergrundbild hinzugefügt, resize EventListener

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,18 @@
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 canvas.width = 900;
 canvas.height = 750;
+
+//Funktion für Automatische Größen-Anpassung des Canvas
+function resize() {
+    // Das Canvas muss unabhängig von der Auflösung die volle Bildschirmhöhe abdecken
+    const height = window.innerHeight - 20; //inner hight muss um 20px verringert werden, weil hier die Scroll-Bar (20px) inkludiert ist
+  
+    // Berechnen der Breite mit der richtigen Skalierung, um das Spielfeld in jeder Auflösung richtig darstellen zu könnnen
+    const ratio = canvas.width / canvas.height;
+    const width = height * ratio;
+  
+    canvas.style.width = width + "px";
+    canvas.style.height = height + "px";
+  }
+  window.addEventListener("load", resize, false); //Immer wenn das Fenster neu geladen wird, wird die resize-Funktion aufgerufen
+  //Hierzu brauchen wir einen EventListener, der prüft ob das Fenster verändert wird und dann die Funktion called
