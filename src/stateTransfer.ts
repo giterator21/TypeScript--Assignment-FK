72e0906b78d92b3eacb404f103572a5e4ac04b82
giterator21 <fel.kopp.21@lehre.mosbach.dhbw.de>
2022-05-11 12:23:07 +0200

    TransferState erstellt, Font nachgeladen

--- src/stateTransfer.ts
+++ src/stateTransfer.ts
@@ -1,5 +1,8 @@
 // --- stateInTransfer--- //
 
+import {GameBasics} from "./index";
+import {InGameState} from "./stateInGame";
+
 export class TransferState {
     public level: number;
     public fontSize: number;
@@ -11,4 +14,21 @@ export class TransferState {
         this.fontColor = 255;
     }
 
+    update(play: GameBasics) {
+        this.fontSize -= 1;
+        this.fontColor -= 1.5;
+    }
+
+    draw(play: GameBasics) {
+        play.ctx.clearRect(0, 0, play.width, play.height);
+        play.ctx.font = this.fontSize + "px Starjhol";
+        play.ctx.textAlign = "center";
+        play.ctx.fillStyle =
+            "rgba(255, " + this.fontColor + ", " + this.fontColor + ", 1)";
+        play.ctx.fillText(
+            "Mach dich bereit für Level " + this.level,
+            play.width / 2,
+            play.height / 2
+        );
+    }
 }
